526a3e70314928f3696a5c6549cca54e8e4923f4
ShepherdSyncs <info@shepherdsyncs.com>
2026-09-05 21:14:39 -0400

    update src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
+if ('serviceWorker' in navigator) { navigator.serviceWorker.register('/sw.js').catch(() => {}); }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
