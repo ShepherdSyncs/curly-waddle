26c40dc33a619fda31992a541a36361de0313889
ShepherdSyncs <info@shepherdsyncs.com>
2026-09-13 15:48:18 -0400

    import AuthenticatedApp from separate file, break circular dep

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { AuthenticatedApp } from '@/components/AuthenticatedApp';
 import React, { useState, useEffect } from 'react';
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
@@ -77,7 +78,7 @@ return host === 'testchurch';
 
 const PUBLIC_PATHS = ['/live', '/give', '/pray', '/portal', '/signup', '/kiosk', '/event-signup', '/login', '/forgot-password', '/reset-password'];
 
-export const AuthenticatedApp = () => {
+const AuthenticatedApp = () => {
 const { isLoadingAuth, authError, isAuthenticated, authChecked, navigateToLogin } = useAuth();
 
 if (isLoadingAuth) {
